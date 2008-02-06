2f1942fae15079e871b6508a049f6ddbae5d371f
dimaexe <dimaexe@cc5033d0-740f-0410-afc7-949910e492f2>
2008-02-06 23:44:57 +0000

    Fixed [#16742]: pass rails environment to script/console
    git-svn-id: svn+ssh://rubyforge.org/var/svn/emacs-rails/trunk@221 cc5033d0-740f-0410-afc7-949910e492f2

--- rails-ruby.el
+++ rails-ruby.el
@@ -173,11 +173,11 @@ See the variable `align-rules-list' for more details.")
 
 (require 'inf-ruby)
 
-(defun run-ruby-in-buffer (cmd buf)
+(defun run-ruby-in-buffer (buf script &optional params)
   "Run CMD as a ruby process in BUF if BUF does not exist."
   (let ((abuf (concat "*" buf "*")))
     (when (not (comint-check-proc abuf))
-      (set-buffer (make-comint buf rails-ruby-command nil cmd)))
+      (set-buffer (make-comint buf rails-ruby-command nil script params)))
     (inferior-ruby-mode)
     (make-local-variable 'inferior-ruby-first-prompt-pattern)
     (make-local-variable 'inferior-ruby-prompt-pattern)
